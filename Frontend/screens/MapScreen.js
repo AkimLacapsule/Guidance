e75f218ae2252d331577052c2e30542350876007
Cameus <hans@MacBook-Pro.local>
2020-08-10 22:39:32 +0200

    headerdumap

--- Frontend/screens/MapScreen.js
+++ Frontend/screens/MapScreen.js
@@ -1,14 +1,17 @@
 import React, { useEffect , useState }from 'react';
-import { Text, View ,StyleSheet} from 'react-native';
+import { Text, View ,StyleSheet ,Image} from 'react-native';
 import MapView , { Marker } from 'react-native-maps';
 import * as Permissions from "expo-permissions";
 import * as Location from 'expo-location';
-
+import { Header } from "react-native-elements";
+import  { Ionicons } from "react-native-vector-icons";
+import { FontAwesome } from '@expo/vector-icons'; 
 
 export default function MapScreen () {
 
     const [latitude,setLatitude] = useState(0);
     const [longitude,setLongitude] = useState(0);
+    console.log(latitude,longitude)
 
 
     useEffect(() => {
@@ -22,12 +25,25 @@ export default function MapScreen () {
         }
      ask()
         }, [])
-        console.log(latitude)
 
     return (
+        <View>
+                  <Header backgroundColor={styles.header.color}  >
+                 <FontAwesome name="user-circle-o" size={24} color="white" />
+                 <Ionicons name="ios-chatboxes" size={24} color="white" />
+           </Header>
+
         <MapView style={styles.Map} region={{latitude:latitude,longitude:longitude}}>
-          <Marker coordinate={{latitude:latitude,longitude:longitude,latitudeDelta:latitude,longitudeDelta:longitude}} title="tu es la    , "   description="tu es la"/>
+        
+          <Marker coordinate={{
+            latitude:latitude,
+            longitude:longitude,
+            latitudeDelta:latitude,
+            longitudeDelta:longitude
+            }}
+             title="tu es la "   description="tu es la"/>
         </MapView>
+        </View>
     )
 }
 
@@ -35,6 +51,11 @@ const styles = StyleSheet.create({
     Map:{
         width:"100%",
         height:"100%"
+    },
+    header:{
+        color:"#4D3D84",
+        flex: 1,
     }
+
 })
 
