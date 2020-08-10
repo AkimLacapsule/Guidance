ab851372d904e2bdb0af597fbdd49e56681599e5
Manon Lecareux <lecareuxm@gmail.com>
2020-08-10 17:36:11 +0200

    début badges

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -4,15 +4,23 @@ var SHA256 = require("crypto-js/sha256");
 var encBase64 = require("crypto-js/enc-base64");
 var mongoose = require('mongoose');
 
+var userModel = require("../models/users"); 
 
+var cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name:"dvx36h3ub",
+  api_key:"125794775985349",
+  api_secret:"ozn8F-OjOcFuMD415z_FY95li2Y"
+});
 
 
 
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/points-counter', async function(req, res, next) {
+  var searchUser = await userModel.findOne({token:"iIaLscNCDgxmM9BAwk1JmR5dh1UPyqdF"});
+  res.json(searchUser);
 });
 
 router.post('/', function(req, res, next) {
